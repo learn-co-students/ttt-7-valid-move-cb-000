0963b8ccd44bd0162ddbc4bc02f3214551c3dd39
Steven McCombe <stevenmccombe@hotmail.co.uk>
2017-08-20 23:28:59 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,18 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  if position_taken?(board, index) == true || index.between?(0,9) == false #if the position is taken or the input number is above 9 it is not a vailid move
+false
+elsif position_taken?(board, index) == false #if the position is free then it is a vailid move
+  true
+end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+
+if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+  true
+end
+end
